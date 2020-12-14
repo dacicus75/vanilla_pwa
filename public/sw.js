8a01c45f5b0efee7c493c529b33b7fe668fbdbe2
dacicus75 <dacicus75@hotmail.com>
2020-12-13 19:53:16 -0500

    changed sw.js  to improve lighthouse score

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,28 @@
 // Cache all the files to make a PWA
-self.addEventListener('install', e => {
+self.addEventListener('install', function(e) {
     e.waitUntil(
-      caches.open(cacheName).then(cache => {
-        // Our application only has two files here index.html and manifest.json
-        // but you can add more such as style.css as your app grows
-        return cache.addAll([
-          './',
-          './index.html',
-          './manifest.json'
-        ]);
-      })
-    );
-  });
+       caches.open('default-cache').then(function(cache) {
+          return cache.addAll([
+             '/',
+             '/index.html',
+             '/manifest.json',
+        ])
+    }).then(function() {
+        return self.skipWaiting();
+    }));
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(self.clients.claim());
+});
   
   // Our service worker will intercept all fetch requests
   // and check if we have cached the file
   // if so it will serve the cached file
-  self.addEventListener('fetch', event => {
+  self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.open(cacheName)
-        .then(cache => cache.match(event.request, { ignoreSearch: true }))
-        .then(response => {
-          return response || fetch(event.request);
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
         })
     );
-  });
+});
